c61e883bab73002c278df4eaa2209c1ae8270b91
Jens Petersen <juhpetersen@gmail.com>
2017-01-07 20:17:33 +0900

    check snap looks right and only output if absolute loc

--- Main.hs
+++ Main.hs
@@ -1,28 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad
 import qualified Data.ByteString.Char8 as B
+import Data.List
 import Data.Maybe
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import System.Environment
 
-streams = ["lts", "nightly"]
-
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [s] | s `elem` streams -> stackageRequest s
     [p] -> stackageRequest $ pkgPath "nightly" p
-    [s, p] -> stackageRequest $ pkgPath s p
-    _ -> error "Usage: stackage-query [snap] [pkg]"
+    [s, p] | checkSnap s -> stackageRequest $ pkgPath s p
+    _ -> error "Usage: stackage-query [lts|nightly] [pkg]"
   where
     pkgPath :: String -> String -> String
     pkgPath s p = s ++ "/package/" ++ p
 
+    checkSnap :: String -> Bool
+    checkSnap s = s `elem` streams || any (`isPrefixOf` s) (map (++ "-") streams) 
+
+    streams = ["lts", "nightly"]
 
 stackageRequest path = do
   mgr <- newManager tlsManagerSettings
-  req <- parseRequest $ "https://www.stackage.org/" ++ path
+  let url = "https://www.stackage.org/"
+  req <- parseRequest $ url ++ path
   hist <- responseOpenHistory req mgr
-  B.putStrLn $ last . catMaybes . map (lookup "Location" . responseHeaders . snd) $ hrRedirects hist
+  let redirs = catMaybes . map (lookup "Location" . responseHeaders . snd) $ hrRedirects hist
+  unless (null redirs) $ do
+    let loc = last redirs
+    when (url `isPrefixOf` B.unpack loc) $
+      B.putStrLn loc
