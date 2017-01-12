8d4c06910c1fa44442d6474b5ba854663f781fd4
Jens Petersen <juhpetersen@gmail.com>
2017-01-12 19:34:53 +0900

    use optparse-applicative; commands for cabal.config and buildplan
    - download of cabal.config uses http-conduit
    - showing buildplan uses stackage-curator
    
    Actually `stackage-build-plan --snapshot lts --format simple`
    can already show buildplan, so heavy dep seems redundant for now.

--- Main.hs
+++ Main.hs
@@ -2,28 +2,65 @@
 
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as L
 import Data.List
 import Data.Maybe
+import qualified Data.Text as T
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Network.HTTP.Simple
+import Options.Applicative
+import Stackage.ShowBuildPlan
 import System.Environment
 import System.Exit
 
+data Command = List String String
+             | Config String
+             | BuildPlan String String
+
+parseString :: String -> Parser String
+parseString lbl = strArgument $ metavar lbl
+
+listParser :: Parser Command
+listParser = List <$> parseString "SNAP" <*> parseString "PKG"
+
+configParser :: Parser Command
+configParser = Config <$> parseString "SNAP"
+
+buildplanParser :: Parser Command
+buildplanParser = BuildPlan <$> parseString "SNAP" <*> parseString "PKG"
+
+commandParser :: ParserInfo Command
+commandParser = info (helper <*> commands) $ progDesc "try --help for more info" where
+    commands = subparser $ mconcat
+      [ command "list"
+        (info listParser (progDesc "Show Stackage SNAP version of PKG"))
+      , command "config"
+        (info configParser (progDesc "Download cabal.config file for SNAP"))
+      , command "buildplan"
+        (info buildplanParser (progDesc "Show SNAP buildplan for PKG"))
+      ]
+
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [p] -> stackageRequest $ pkgPath "nightly" p
-    [s, p] | checkSnap s -> stackageRequest $ pkgPath s p
-    _ -> error "Usage: stackage-query [lts|nightly] [pkg]"
+  cmd <- execParser commandParser
+  case cmd of
+    List s p -> stackageRequest $ pkgPath s p
+    Config s -> stackageConfig s
+    BuildPlan s p -> stackageBuildplan s p
   where
     pkgPath :: String -> String -> String
-    pkgPath s p = s ++ "/package/" ++ p
+    pkgPath s p =
+      if checkSnap s
+      then s ++ "/package/" ++ p
+      else error "SNAP should be start with 'lts' or 'nightly'"
 
     checkSnap :: String -> Bool
-    checkSnap s = s `elem` streams || any (`isPrefixOf` s) (map (++ "-") streams) 
-
-    streams = ["lts", "nightly"]
+    checkSnap s =
+      s `elem` streams ||
+      any (`isPrefixOf` s) (map (++ "-") streams)
+      where
+        streams = ["lts", "nightly"]
 
 stackageRequest path = do
   mgr <- newManager tlsManagerSettings
@@ -40,3 +77,12 @@ stackageRequest path = do
       else giveup
   where
     giveup = die $ path ++ " not found"
+
+stackageConfig snap = do
+  req <- parseRequest $ "https://www.stackage.org/" ++ snap ++ "/cabal.config"
+  response <- httpLBS req
+  L.writeFile "cabal.config" $ getResponseBody response
+
+stackageBuildplan :: String -> String -> IO ()
+stackageBuildplan snap pkg =
+  T.unpack . toSimpleText <$> getBuildPlan defaultSettings [mkPackageName $ T.pack pkg] >>= putStr
--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 Small tool that queries (returns url of) package versions on Stackage.
 
-This is just a simple that looks at Stackage's HTTP redirects to determine
-the package version URL.
+This is just a simple hack that looks at Stackage's HTTP redirects
+to determine the package version URL.
 
-Usage:
+Example usage:
 
 ```
-$ stackage-query lts pandoc
+$ stackage list lts pandoc
 https://www.stackage.org/lts-7.14/package/pandoc-1.17.1
+$ stackage config lts
+$ stackage buildplan lts pandoc
+array 0.5.1.1
+:
+pandoc 1.17.1
 ```
