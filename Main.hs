d0a05b095d64aa01327e996bd56a0d9c4e56fc97
Jens Petersen <juhpetersen@gmail.com>
2017-04-18 19:18:05 +0900

    Snapshot replaces SnapshotType; implement findSnap
    Shapshot has Show and Read instances
    
    findSnap searches for snap yaml file

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as L
+import Data.Char (isDigit)
 import Data.List
 import qualified Data.Map.Strict (assocs, lookup)
 import Data.Maybe
@@ -24,22 +25,45 @@ import Data.Foldable (traverse_)
 import Data.Yaml hiding (Parser)
 import Distribution.Package (PackageName(..), unPackageName)
 
-data Args = Args Command
+newtype Args = Args Command
 
 newtype UrlOption = UrlOption {showurl :: Bool}
 
 newtype ConsumerOption = ConsumerOption {threshold :: Int}
 
-data Command = List UrlOption SnapshotType [String]
-             | Config UrlOption SnapshotType
-             | Ghc SnapshotType
-             | Core SnapshotType
-             | Tools SnapshotType
-             | Packages SnapshotType
-             | Consumers ConsumerOption SnapshotType
-             | Package SnapshotType String
-             | Users SnapshotType String
-             | Github SnapshotType String
+data Snapshot = LatestNightly | LatestLTS | NightlySnap String | LtsSnap String | LtsMajor String
+
+instance Show Snapshot where
+  show LatestNightly = "nightly-2017-04-12"
+  show LatestLTS = "lts-8.9"
+  show (NightlySnap date) = "nightly-" ++ date
+  show (LtsSnap ver) = "lts-" ++ ver
+  show (LtsMajor ver) = "lts-" ++ ver
+
+instance Read Snapshot where
+  readsPrec _ "nightly" = [(LatestNightly,"")]
+  readsPrec _ "lts" = [(LatestLTS,"")]
+  readsPrec _ input | "nightly-" `isPrefixOf` input =
+    let (date,rest) = span (\ c -> isDigit c || c == '-') $ removePrefix "nightly-" input in
+       [(NightlySnap date,rest)]
+  readsPrec _ input | "lts-" `isPrefixOf` input =
+    let (ver,rest) = span (\ c -> isDigit c || c == '.') $ removePrefix "lts-" input in
+      [(if '.' `elem` ver then LtsSnap ver else LtsMajor ver,rest)]
+  readsPrec _ _ = []
+
+removePrefix :: String -> String-> String
+removePrefix pref orig = fromMaybe orig (stripPrefix pref orig)
+
+data Command = List UrlOption Snapshot [String]
+             | Config UrlOption Snapshot
+             | Ghc Snapshot
+             | Core Snapshot
+             | Tools Snapshot
+             | Packages Snapshot
+             | Consumers ConsumerOption Snapshot
+             | Package Snapshot String
+             | Users Snapshot String
+             | Github Snapshot String
 
 urlParser :: Parser UrlOption
 urlParser = UrlOption <$> switch
@@ -47,21 +71,13 @@ urlParser = UrlOption <$> switch
 
 consumeParser :: Parser ConsumerOption
 consumeParser = ConsumerOption <$> option auto
-  (long "minimum" <> short 'm' <> metavar "THRESHOLD" <> value 5 <> help "Show packages with more than MINIMUM consumers (default 5)")
+  (long "minimum" <> short 'm' <> metavar "THRESHOLD" <> value 5 <> help "Show packages with at least THRESHOLD consumers (default 5)")
 
 parseString :: String -> Parser String
 parseString lbl = strArgument $ metavar lbl
 
-parseSnap :: Parser SnapshotType
-parseSnap = argument snap $ metavar "SNAP"
-  where
-    snap :: ReadM SnapshotType
-    snap = maybeReader readSnap
-
-    readSnap :: String -> Maybe SnapshotType
-    readSnap "nightly" = Just STNightly
-    readSnap "lts" = Just $ STLTS 8 9
-    readSnap _ = Nothing
+parseSnap :: Parser Snapshot
+parseSnap = argument auto $ metavar "SNAP"
 
 commandParser :: Parser Command
 commandParser =
@@ -121,13 +137,13 @@ main = do
 topurl :: String
 topurl = "https://www.stackage.org/"
 
-stackageList :: UrlOption -> SnapshotType -> [String] -> IO ()
+stackageList :: UrlOption -> Snapshot -> [String] -> IO ()
 stackageList opts s ps = do
   mgr <- newManager tlsManagerSettings
   mapM_ (sendReq mgr) ps
   where
     sendReq mgr p = do
-      let pkgurl = topurl ++ showSnap s </> "package/"
+      let pkgurl = topurl ++ show s </> "package/"
       req <- parseRequest $ pkgurl ++ p
       hist <- responseOpenHistory req mgr
       let redirs = mapMaybe (lookup "Location" . responseHeaders . snd) $ hrRedirects hist
@@ -139,31 +155,28 @@ stackageList opts s ps = do
           then putStrLn (if showurl opts then loc else takeFileName loc)
           else giveup p
 
-    giveup u = die $ u ++ " not found in " ++ showSnap s
+    giveup u = die $ u ++ " not found in " ++ show s
 
 
-stackageConfig :: UrlOption -> SnapshotType -> IO ()
+stackageConfig :: UrlOption -> Snapshot -> IO ()
 stackageConfig opts snap = do
-  let url = topurl ++ showSnap snap </> "cabal.config"
+  let url = topurl ++ show snap </> "cabal.config"
   req <- parseRequest url
   response <- httpLBS req
   L.writeFile "cabal.config" $ getResponseBody response
   when (showurl opts) $
     putStrLn url
 
-getBuildPlan :: SnapshotType -> IO BuildPlan
+getBuildPlan :: Snapshot -> IO BuildPlan
 getBuildPlan snap = do
   home <- getHomeDirectory
   let configDir = home </> ".stackage-query"
-  haveDir <- doesDirectoryExist configDir
-  unless haveDir $ createDirectory configDir
+  createDirectoryIfMissing False configDir
   let project = snapProject snap
       projectDir = configDir </> show project
   haveProj <- doesDirectoryExist projectDir
   unless haveProj $ cloneProject configDir project
-  let yaml = projectDir </> showSnap snap ++ ".yaml"
-  haveYaml <- doesFileExist yaml
-  when (not haveYaml) $ updateProject projectDir
+  yaml <- findSnap True projectDir snap
   ebp <- decodeFileEither yaml
   either (error . prettyPrintParseException) return ebp
 
@@ -173,14 +186,24 @@ instance Show Project where
   show LTS = "lts-haskell"
   show Nightly = "stackage-nightly"
 
-snapProject :: SnapshotType -> Project
-snapProject (STLTS _ _) = LTS
+snapProject :: Snapshot -> Project
+snapProject LatestLTS = LTS
+snapProject (LtsSnap _) = LTS
+snapProject (LtsMajor _) = LTS
 snapProject _ = Nightly
 
-showSnap :: SnapshotType -> String
-showSnap STNightly = "nightly-2017-04-12"
-showSnap (STNightly2 date) = "nightly-" ++ show date
-showSnap (STLTS major minor) = "lts-" ++ show major ++ "." ++ show minor
+findSnap :: Bool -> FilePath -> Snapshot -> IO FilePath
+findSnap update dir snap = do
+  fs <- sort . filter (show snap `isPrefixOf`) <$> getDirectoryContents dir
+  if null fs
+    then
+    if update
+      then do
+      updateProject dir
+      findSnap False dir snap
+      else
+      error $ "Snap " ++ show snap ++ " not found"
+    else return $ dir </> last fs
 
 cmd_ :: String -> [String] -> IO ()
 cmd_ c args = do
@@ -196,57 +219,57 @@ cloneProject dir proj = do
   cmd_ "git" ["-C", dir, "clone", url]
 
 updateProject :: FilePath -> IO ()
-updateProject dir = do
+updateProject dir =
   cmd_ "git" ["-C", dir, "pull"]
 
-buildplanGHC :: SnapshotType -> IO ()
+buildplanGHC :: Snapshot -> IO ()
 buildplanGHC snap = do
   bp <- getBuildPlan snap
   let si = bpSystemInfo bp
   putStrLn $ (showVersion . siGhcVersion) si
 
-buildplanCore :: SnapshotType -> IO ()
+buildplanCore :: Snapshot -> IO ()
 buildplanCore snap = do
   bp <- getBuildPlan snap
   let si = bpSystemInfo bp
   traverse_ (putStrLn . showPkgVer) $ (Data.Map.Strict.assocs . siCorePackages) si
 
-buildplanTools :: SnapshotType -> IO ()
+buildplanTools :: Snapshot -> IO ()
 buildplanTools snap = do
   bp <- getBuildPlan snap
   traverse_ (putStrLn . showPkgVer) $ bpTools bp
 
-buildplanPackages :: SnapshotType -> IO ()
+buildplanPackages :: Snapshot -> IO ()
 buildplanPackages snap = do
   bp <- getBuildPlan snap
-  traverse_ (putStrLn . showPkgVer) $ Data.Map.Strict.assocs $ fmap ppVersion $ bpPackages bp
+  traverse_ (putStrLn . showPkgVer) $ Data.Map.Strict.assocs $ ppVersion <$> bpPackages bp
 
-buildplanConsumers :: ConsumerOption -> SnapshotType -> IO ()
+buildplanConsumers :: ConsumerOption -> Snapshot -> IO ()
 buildplanConsumers opts snap = do
   bp <- getBuildPlan snap
-  traverse_ putPkgConsumption $ Data.Map.Strict.assocs $ fmap ppUsers $ bpPackages bp
+  traverse_ putPkgConsumption $ Data.Map.Strict.assocs $ ppUsers <$> bpPackages bp
   where
     putPkgConsumption :: (PackageName, Set.Set PackageName) -> IO ()
     putPkgConsumption (p, users) = do
       let n = length users
       when (n >= threshold opts) $
-        putStrLn $ (show n) ++ " " ++ unPackageName p
+        putStrLn $ show n ++ " " ++ unPackageName p
 
-evalPackageBuildPlan :: SnapshotType -> String -> (PackagePlan -> String) -> IO ()
+evalPackageBuildPlan :: Snapshot -> String -> (PackagePlan -> String) -> IO ()
 evalPackageBuildPlan snap pkg expr = do
   bp <- getBuildPlan snap
   let mpkgplan = Data.Map.Strict.lookup (PackageName pkg) $ bpPackages bp
   putStrLn $ maybe "Package not found" expr mpkgplan
 
-buildplanPackage :: SnapshotType -> String -> IO ()
+buildplanPackage :: Snapshot -> String -> IO ()
 buildplanPackage snap pkg =
   evalPackageBuildPlan snap pkg (showVersion . ppVersion)
 
-buildplanUsers :: SnapshotType -> String -> IO ()
+buildplanUsers :: Snapshot -> String -> IO ()
 buildplanUsers snap pkg =
   evalPackageBuildPlan snap pkg (unwords . Set.elems . Set.map unPackageName . ppUsers)
 
-buildplanGithub :: SnapshotType -> String -> IO ()
+buildplanGithub :: Snapshot -> String -> IO ()
 buildplanGithub snap pkg =
   evalPackageBuildPlan snap pkg (unwords . Set.elems . Set.map T.unpack . ppGithubPings)
 
